d780d65d27cd4cdca5a3fe9e63704f21c325589b
Kunal Mehrotra <kunalmehrotra@Kunals-MacBook-Pro.local>
2022-11-29 00:37:19 -0500

    fix url

--- src/services/auth-services.js
+++ src/services/auth-services.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-// const BASE_URL = "https://fse5500-node-app.herokuapp.com/api";
 // const BASE_URL = "http://localhost:4000/api";
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
